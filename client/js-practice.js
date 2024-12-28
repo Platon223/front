ea20bff3717ec7ca5d3880afa0fa10f54747613d
Platon223 <platon.tikhnenko@gmail.com>
2024-12-28 13:43:02 -0600

    Update js-practice.js

--- client/js-practice.js
+++ client/js-practice.js
@@ -258,7 +258,7 @@ function submit() {
           let html = `<div class="stuff1"> <div class="col-lg"> <div class="card text-bg-secondary mb-3" style="width: 18rem;">
         <img src="" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${nm}  $ ${price}</h5>
+          <h5 class="card-title">${pl}  $ ${price}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         <ul class="list-group list-group-flush">
