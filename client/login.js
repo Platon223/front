a8d79404b8d07eb96741d2c7db1219a9c4368636
Platon223 <platon.tikhnenko@gmail.com>
2025-01-20 16:07:37 -0600

    Create login.js

---
+++ client/login.js
@@ -0,0 +1,75 @@
+const socket = io('https://store-4-rc42.onrender.com');
+
+document.querySelector('.dash').innerHTML = localStorage.getItem('dash-name');
+
+
+socket.on('see-accounts', data => {
+    console.log(data);
+})
+
+
+socket.on('test', data => {
+    console.log(data);
+})
+
+socket.on('launch-accounts', usernames => {
+    console.log(usernames);
+})
+
+
+
+function check() {
+
+    const name = document.querySelector('.username').value;
+
+   
+    socket.on('launch-accounts', (usernames) => {
+        console.log(usernames);
+
+      
+
+        let nameInUse = false;
+        usernames.forEach(ur => {
+            if (ur.nm === name) {
+                nameInUse = true;
+            }
+        });
+
+    
+
+        if (nameInUse) {
+            console.log('This name is already in use.');
+            location.reload();
+        } else {
+            console.log('You are good to go.');
+
+            const loged = true;
+
+            const user = { nm: name, loged: loged };
+            socket.emit('send-acc', user);
+
+            if(name !== localStorage.getItem('dash-name')) {
+                console.log('hello');
+                const accName = localStorage.getItem('dash-name');
+                socket.emit('delete-acc', accName);
+            } 
+
+
+            const dashBoard = name;
+            localStorage.setItem('dash-name', dashBoard);
+
+       
+
+            document.querySelector('.dash').innerHTML = localStorage.getItem('dash-name');
+            location.reload();
+            
+
+        }
+        
+
+    });
+  
+
+
+    socket.emit('request-accounts');
+}
