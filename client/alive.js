23b868aaaf4ceca0bfec9ca0083bc8afa638580f
Platon223 <platon.tikhnenko@gmail.com>
2025-02-17 15:03:08 -0600

    Update alive.js

--- client/alive.js
+++ client/alive.js
@@ -1,5 +1,5 @@
 setInterval(() => {
     fetch("https://store-7.onrender.com/api/products")
         .then(() => console.log("awake"))
-        .catch((err) => console.log("error", err); 
+        .catch((err) => console.log("error", err)); 
 }, 60000);
