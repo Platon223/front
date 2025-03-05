acd1c4ce40ea0090d6a9cd2e8ea8dbc446cd9885
Platon223 <platon.tikhnenko@gmail.com>
2025-03-04 19:51:49 -0600

    Update js-practice.js

--- client/js-practice.js
+++ client/js-practice.js
@@ -68,10 +68,19 @@ async function fetchProducts() {
     products.forEach(pr => {
         const prElement = document.createElement('div');
 
-        prElement.innerHTML = `<div>
-            <h1>${pr.nm}</h1>
-            <a href="https://store-7.onrender.com/products/${pr.nm}" target="_blank">See Description</a>
-        </div>`;
+        prElement.innerHTML = `<div class="product-card">
+        <img
+          src="https://images.unsplash.com/photo-1741087562365-d0bf6e6fd7ef?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Product Image"
+          class="product-img"
+        />
+        <div class="product-info">
+          <h2 class="product-title">${pr.nm}</h2>
+          <p class="product-description"> <a href="https://store-7.onrender.com/products/${pr.nm}">Short description of product.</a> </p>
+          <p class="product-price">${pr.price}</p>
+          <a href="#" class="buy-btn">Buy Now</a>
+        </div>
+      </div>`;
 
       list.appendChild(prElement);
     });
